6020f6783c82f17a2553eb8489db09bab834f073
Tatiana <tmv0001@mail.ru>
2023-03-19 16:28:23 +0500

    Initional commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 9054;
-        int sum_for_miles = 20;
-        System.out.println(ticket_price / sum_for_miles);
+        int ticketPrice = 9054;
+        int sumForMiles = 20;
+        System.out.println(ticketPrice / sumForMiles);
     }
 }
